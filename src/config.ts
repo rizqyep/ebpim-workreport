f64939e1371671145d9acad87033210c5a18c40b
Rep <rizqyepr@gmail.com>
2025-10-10 16:26:29 +0700

    feat: update progress 10/10

--- src/config.ts
+++ src/config.ts
@@ -2,12 +2,12 @@ export const PHASE_START_DATE = "Oct 6 2025";
 export const PHASE_END_DATE = "Oct 21 2025";
 
 export const TOTAL_STORY_POINT = 19;
-export const TOTAL_STORY_COMPLETED = 6;
+export const TOTAL_STORY_COMPLETED = 11;
 
 export const STORY_POINTS_BY_STATUS = [
-  { status: "To Do", points: 11 },
+  { status: "To Do", points: 6 },
   { status: "In Progress", points: 2 },
   { status: "Need Review", points: 0 },
   { status: "In Review", points: 0 },
-  { status: "Done", points: 6 },
+  { status: "Done", points: 11 },
 ];
--- src/data/burndown.ts
+++ src/data/burndown.ts
@@ -5,7 +5,7 @@ export const ACTUAL_BURNDOWN: ActualPoint[] = [
   { date: "10/7/2025", storyPoints: 17 },
   { date: "10/8/2025", storyPoints: 17 },
   { date: "10/9/2025", storyPoints: 13 },
-  { date: "10/10/2025", storyPoints: null },
+  { date: "10/10/2025", storyPoints: 8 },
   { date: "10/11/2025", storyPoints: null },
   { date: "10/12/2025", storyPoints: null },
   { date: "10/13/2025", storyPoints: null },
