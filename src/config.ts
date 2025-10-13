c25a9aa2921a0403436fc68eee89d021ac3c2fb5
Rep <rizqyepr@gmail.com>
2025-10-13 17:14:52 +0700

    feat: update progress 13/10

--- src/config.ts
+++ src/config.ts
@@ -2,12 +2,12 @@ export const PHASE_START_DATE = "Oct 6 2025";
 export const PHASE_END_DATE = "Oct 21 2025";
 
 export const TOTAL_STORY_POINT = 19;
-export const TOTAL_STORY_COMPLETED = 11;
+export const TOTAL_STORY_COMPLETED = 13;
 
 export const STORY_POINTS_BY_STATUS = [
-  { status: "To Do", points: 6 },
-  { status: "In Progress", points: 2 },
+  { status: "To Do", points: 3 },
+  { status: "In Progress", points: 3 },
   { status: "Need Review", points: 0 },
   { status: "In Review", points: 0 },
-  { status: "Done", points: 11 },
+  { status: "Done", points: 13 },
 ];
